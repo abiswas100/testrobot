4a597ab9740f3c8cde0e998abec68c88278ae1ae
Avhi <abiswas3@huskers.unl.edu>
2022-04-14 03:19:13 -0500

    launch file working

--- test_catkin/src/testrobots/scripts/human_detection_oops.py
+++ test_catkin/src/testrobots/scripts/human_detection_oops.py
@@ -10,9 +10,10 @@ from sensor_msgs.msg import PointCloud2 as pc2
 import csv
 import cv2
 from cv_bridge import CvBridge
-import yolo as Yolo
+# import yolo as Yolo
 import numpy as np
 import os
+import time
 
 from testrobots.msg import H_detection
 from testrobots.msg import stop  
@@ -36,11 +37,11 @@ class Detection(object):
         
         #initialize csv file
         self.path = os.getcwd()
-        self.path = self.path+"/human_motion.csv"
+        self.csvpath = self.path+"/human_motion.csv"
         # open the file in the write mode
         
         header = ['center_x', 'center_y', 'Distance']
-        self.csv_file = open(self.path, 'w')
+        self.csv_file = open(self.csvpath, 'w')
         self.writer = csv.writer(self.csv_file)
         self.writer.writerow(header)
         
@@ -59,7 +60,7 @@ class Detection(object):
         '''
         msg = H_detection()
         msg.signal = -1
-        yolo_output, object_label, center_pixels = Yolo.Yolo_imp(cv_img)
+        yolo_output, object_label, center_pixels = self.Yolo_imp(cv_img)
         
         try:
             self.center_pixel = center_pixels[0]
@@ -83,28 +84,36 @@ class Detection(object):
         self.msg_pub.publish(msg)
         self.pub.publish(output)
         
-        self.past_center = self.center_pixel
-      
+        try:
+            self.past_center = self.center_pixel
+        except AttributeError: pass
+        
     def human_motion_tracking(self, cv_img):
-        center_x = self.center_pixel[1]
-        center_y = self.center_pixel[0]
-        
-        past_center_x = self.past_center_pixel[1]
-        past_center_y = self.past_center_pixel[0]
-        
-        start_point = (center_x,center_y)
-        
-        end_point = (past_center_x, past_center_y)
-        
-        #red color in BGR
-        color = (0, 0 , 255)
-        
-        thickness = 2
-        
-        image = cv2.arrowedLine(cv_img, start_point, end_point,
-                                     color, thickness)
-        self.pub.publish(image)
+        try:
+            center_x = self.center_pixel[1]
+            center_y = self.center_pixel[0]
+            
+            past_center_x = self.past_center_pixel[1]
+            past_center_y = self.past_center_pixel[0]
+            
+            start_point = (center_x,center_y)
+            
+            end_point = (past_center_x, past_center_y)
+            
+            #red color in BGR
+            color = (0, 0 , 255)
+            
+            thickness = 2
+            
+            image = cv2.arrowedLine(cv_img, start_point, end_point,
+                                        color, thickness)
+            
+            output = bridge.cv2_to_imgmsg(image)
+            self.pub.publish(output)
 
+        except AttributeError or IndexError:
+            output = bridge.cv2_to_imgmsg(cv_img)
+            self.pub.publish(output)
         
     def DepthCamSub(self,depth_data):
         depth_cv_img =  bridge.imgmsg_to_cv2(depth_data)
@@ -121,9 +130,7 @@ class Detection(object):
             msg.stop = -1            
             
             #changing Nan Values to 0
-            if depth == "nan":
-                depth = 0
-            
+            if depth == "nan": depth = 0
             
             data_to_write = [center_x,center_y,depth]
 
@@ -132,6 +139,7 @@ class Detection(object):
 
             print("distance of human in depthcam", depth_cv_img[self.center_pixel[1]][self.center_pixel[0]])
             
+            #checking safety of robot and human
             if depth <= 1.5 : 
                 rospy.logwarn("Human too close ... Stop Immediately")
                 msg.stop = 1
@@ -142,7 +150,116 @@ class Detection(object):
         except AttributeError or IndexError:
             print("no centers in depth")
                 
+
+    def Yolo_imp(self,img_data): 
+        start_time = time.perf_counter ()
+        current_path = os.getcwd()
+        scripts_path = "/home/tran/testrobot/test_catkin/src/testrobots/scripts"
+        
+        os.chdir(scripts_path)
+        
+        cfg = scripts_path+"/yolov3.cfg"
+        weights = scripts_path + "/yolov3.weights"
+        
+        net = cv2.dnn.readNet(cfg,weights)
+        classes = []
+
+        with open('coco.names', 'r') as f:
             
+            classes = f.read().splitlines()
+
+        # img_name = name1 = input("Enter name of the image file: ")
+        # img_data  = cv2.imread('image.jpeg')
+        # print(img_data)
+        height,width,_ = img_data.shape
+        # print(height,width)
+        blob = cv2.dnn.blobFromImage(img_data, 1/255, (256, 256), (0,0,0), swapRB=False, crop=False)
+
+
+        net.setInput(blob)
+
+        output_layers_names = net.getUnconnectedOutLayersNames()
+
+        layerOutputs = net.forward(output_layers_names)
+
+        boxes = []
+        confidences = []
+        class_ids = []
+        center_pixels = []
+        for output in layerOutputs:
+            for detection in output:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+                '''
+                    scale the bounding box coordinates back relative to the
+                    size of the image, keeping in mind that YOLO actually
+                    returns the center (x, y)-coordinates of the bounding
+                    box followed by the boxes' width and height
+                '''
+                if confidence > 0.5:                                
+                    center_x = int(detection[0]*width)
+                    center_y = int(detection[1]*height)
+                    w = int(detection[2]*width)
+                    h = int(detection[3]*height)
+                    '''
+                        use the center (x, y)-coordinates to derive the top and left corner of the bounding box
+                    '''
+                    x = int(center_x - w/2)
+                    y = int(center_y - h/2)
+                    
+                    boxes.append([x,y,w,h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+                    # print(class_id)
+                    if class_id == 0:
+                        center_pixels.append([center_x,center_y])
+        print("---------------------------------------------------")
+        print("center pixels in yolo",center_pixels)
+        # print(len(boxes))
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        # print(indexes.flatten())
+        object_label = ""
+        
+        font = cv2.FONT_HERSHEY_PLAIN
+        colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+        if len(indexes)>0:
+
+            for i in indexes.flatten():
+                x,y,w,h = boxes[i]
+                label = str(classes[class_ids[i]])
+                confidence = str(round(confidences[i], 2))
+                area = 0
+                print("")
+                
+                print("label -",label,
+                ", confidence", confidence,
+                ", area of Bounding Box  - ",w*h)
+
+                
+                # if label == 'person':
+                #     area = w*h
+
+                color = colors[i]
+                cv2.rectangle(img_data,(x,y), (x+w, y+h), color, 2)
+                cv2.putText(img_data, label + " " + confidence, (x, y+20), font, 2, (255,255,255), 2)
+            
+        try:
+            object_label = label
+        except UnboundLocalError: 
+            print("no label")
+
+        # object_label = "person"
+        end_time = time.perf_counter ()
+        print("")
+        # print(end_time - start_time, "seconds")
+        cv2.imwrite('yolo_img.jpeg', img_data)
+        os.chdir(current_path)
+        return img_data, object_label, center_pixels 
+
+
 def main():
     rospy.init_node('Human_Detection', anonymous=False)
     sn = Detection()
