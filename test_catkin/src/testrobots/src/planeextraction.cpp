805ff10053b24061710e624b9ba4a2e3c8c572eb
Apala <apalapramanikraka@gmail.com>
2022-05-27 01:09:06 -0500

    planeextraction creates finalfloorlesspcd

--- test_catkin/src/testrobots/src/planeextraction.cpp
+++ test_catkin/src/testrobots/src/planeextraction.cpp
@@ -47,35 +47,54 @@ enum class Normal
   eZ
 };
 const Normal normal = Normal::eY;
+pcl::PCDWriter m_writer;
+// PCL plane extraction - a hard threshold, especially for if something goes wrong or lots of small (insignificant) planes
+const unsigned MAX_PLANE_COUNT(8);
+const double PLANE_EXTRACTION_CONTINUE_THRESHOLD(0.30); // While 30% of the original cloud is still there
+unsigned m_pipelineStepCount =10; 
+
+const double minThreshold = 0.97; 
+std::string printStepCount() //const std::string SegmentationPipeline::printStepCount() //const
+{
+  std::stringstream ss;
+  ss << std::setfill('0') << std::setw(2) << m_pipelineStepCount;
+  return ss.str();
+}
+
+std::string printStepCount(unsigned addition) //const
+{
+  std::stringstream ss;
+  ss << std::setfill('0') << std::setw(2) << (m_pipelineStepCount + addition);
+  return ss.str();
+}
 
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
 
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 { 
-    // pcl::PCLPointCloud2* m_cloud = new pcl::PCLPointCloud2;
-    // pcl::PCLPointCloud2ConstPtr cloudPtr(m_cloud);
-    pcl::PCLPointCloud2Ptr p_cloud(new pcl::PointCloud2());
-    pcl::PCLPointCloud2 m_cloud;
-    pcl_conversions::toPCL(*cloud_msg,m_cloud);
-
-    *p_cloud = *cloud_msg;
-
-
     ROS_INFO_STREAM("getting here 1");
-    pcl::PCLPointCloud2 final_planeless_cloud;
-    pcl::PCLPointCloud2 cloud_for_plane_extraction;
-    copyPointCloud(m_cloud, final_planeless_cloud);
-    ROS_INFO_STREAM("getting here 2");
-    copyPointCloud(m_cloud, cloud_for_plane_extraction);
-    ROS_INFO_STREAM("getting here 3");
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*cloud_msg,pcl_pc2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr m_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr final_planeless_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(pcl_pc2,*m_cloud);
+
+    copyPointCloud(*m_cloud, *final_planeless_cloud);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_for_plane_extraction(new pcl::PointCloud<pcl::PointXYZ>);
+    copyPointCloud(*m_cloud, *cloud_for_plane_extraction);
     
-    pcl::PCLPointCloud2 cloud_plane;
-    pcl::PCLPointCloud2 cloud_f;
+    // ROS_INFO_STREAM("getting here 2");
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f(new pcl::PointCloud<pcl::PointXYZ>);
+    
+    ROS_INFO_STREAM("getting here 3");
     
     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices());
 
-        // Create the segmentation object
+    // Create the segmentation object
     pcl::SACSegmentation<pcl::PointXYZ> seg;
     
     // Optional
@@ -89,20 +108,142 @@ void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     ROS_INFO_STREAM("getting here 4");
 
 
-    // Create the extracting object (extraction based on indices)
+    // // Create the extracting object (extraction based on indices)
     pcl::ExtractIndices<pcl::PointXYZ> extract;
         
-    //Keep the removed points as NaN values to maintain the structure of the cloud
+    // //Keep the removed points as NaN values to maintain the structure of the cloud
     extract.setKeepOrganized(true);
         
     unsigned planeCount =0;
-    const unsigned numOriginalPoints = cloud_for_plane_extraction.width;   //How many points we originally started with
-    const unsigned numOriginalPoints2 = cloud_for_plane_extraction.height;
-    const unsigned numOriginalPoints3 = numOriginalPoints * numOriginalPoints2;
-    unsigned numPointsExtracted =0;   //Keep track of how many points have so far been extracted
-    std::cout << numOriginalPoints3 << std::endl;    
+    
+    const unsigned numOriginalPoints = cloud_for_plane_extraction->size();   //How many points we originally started with
+    unsigned numPointsExtracted =0;   //Keep track of how many points have so far been extracted  
+    std::cout << numOriginalPoints << std::endl;
     ROS_INFO_STREAM("getting here 5");
 
+      //Do until we extract a certain percentage of the points -or-
+    // until the maximum number of planes is met, which is checked at the end of this loop
+  while(numPointsExtracted < numOriginalPoints * (1.0 - PLANE_EXTRACTION_CONTINUE_THRESHOLD)) {
+      
+    // Segment the largest planar component from the remaining cloud
+    seg.setInputCloud(cloud_for_plane_extraction);
+    seg.segment(*inliers, *coefficients);
+    if(inliers->indices.size () == 0) {
+      std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+      break;
+    }
+    
+    //Accumulate the number of points extracted for this plane
+    numPointsExtracted += inliers->indices.size();
+      
+    // Extract the inliers into a cloud that contains exactly (and only) the plane
+    extract.setInputCloud(cloud_for_plane_extraction);
+    extract.setIndices(inliers);
+    extract.setNegative(false);
+    extract.filter(*cloud_plane);
+
+    std::cout << "   - Plane #" << planeCount << std::endl;
+    std::cout << "     PointCloud representing the planar component: "
+              << inliers->indices.size() << " data points." << std::endl;
+
+    std::stringstream ss;
+    // ss << m_baseName << "_step" << printStepCount() << "_plane_" << planeCount << ".pcd";
+    ss << "h_step" << printStepCount() << "_plane_" << planeCount << ".pcd";
+    m_writer.write<pcl::PointXYZ>(ss.str(), *cloud_plane, false);
+
+    ////////////
+    //Calculate the plane normals - used to determine if the plane is a floor plane
+
+    // Create the normal estimation class, and pass the input dataset to it
+    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+
+    //Calculate the least-squares plane value
+    float nx, ny, nz, curvature;
+    ne.computePointNormal(*cloud_for_plane_extraction, inliers->indices, nx, ny, nz, curvature);
+
+    //Now calc each normal
+    ne.setInputCloud(cloud_plane);
+
+    // Create an empty kdtree representation, and pass it to the normal estimation object.
+    // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ> ());
+    ne.setSearchMethod (tree);
+    
+    // Output datasets
+    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
+
+    // Use all neighbors in a sphere of radius 3cm
+    ne.setRadiusSearch(0.03);
+    
+    // Compute the features
+    ne.compute(*cloud_normals);
+    std::cout << "     Least-squares plane fit: (" << nx << ", " << ny << ", " << nz << ")"
+              << ", curvature = " << curvature << std::endl;
+
+    bool minThresholdMet = false;
+    if((normal == Normal::eX) && (fabs(nx) > minThreshold))
+      minThresholdMet = true;
+    if((normal == Normal::eY) && (fabs(ny) > minThreshold))
+      minThresholdMet = true;
+    if((normal == Normal::eZ) && (fabs(nz) > minThreshold))
+      minThresholdMet = true;
+
+    if(minThresholdMet)
+    {
+      std::cout << endl;
+      std::cout << "     ==> Minimum threshold of " << minThreshold << " met. Extracting this plane (#" << planeCount << "). <==" << std::endl;
+      std::cout << endl;
+      
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_minus_plane(new pcl::PointCloud<pcl::PointXYZ>);
+      
+      // Create another filtering object 
+      pcl::ExtractIndices<pcl::PointXYZ> normalPlaneExtractor;
+      normalPlaneExtractor.setInputCloud(final_planeless_cloud);
+      normalPlaneExtractor.setIndices(inliers);
+      normalPlaneExtractor.setNegative(true);
+    
+      //Keep the removed points as NaN values to maintain the structure of the cloud
+      normalPlaneExtractor.setKeepOrganized(true);
+
+      normalPlaneExtractor.filter(*cloud_minus_plane);
+
+      std::cout << "final_planeless_cloud" << std::endl;
+      std::cout << "width = " << final_planeless_cloud->width << std::endl;
+      std::cout << "height = " << final_planeless_cloud->height << std::endl;
+
+      std::cout << "cloud_minus_plane" << std::endl;
+      std::cout << "width = " << cloud_minus_plane->width << std::endl;
+      std::cout << "height = " << cloud_minus_plane->height << std::endl;
+
+      //If this file already exists (i.e., part of the floor was removed in an earlier pass),
+      // it will be over written here, and so just the updated, most recent version (with
+      // the least amount of floor plane) will remain.
+      std::stringstream ss;
+      ss << "i_step" << printStepCount(1) << "_floorless_cloud.pcd";
+      m_writer.write<pcl::PointXYZ>(ss.str(), *cloud_minus_plane, false);
+      
+      //Make this is our new cloud
+      copyPointCloud(*cloud_minus_plane, *final_planeless_cloud);
+    }
+
+    //Report the number of points extracted after this plane extraction
+    std::cout << "     " << numPointsExtracted << " points extracted so far (of a total "
+              << numOriginalPoints << ")." << std::endl << std::endl;
+    
+    // Remove the plane from our working cloud
+    extract.setNegative(true);
+    extract.filter(*cloud_f);
+    cloud_for_plane_extraction.swap(cloud_f);
+
+    //Increment the current plane count
+    ++planeCount;
+    
+    //Put in a max number of planes so if something goes wrong, we don't write a billion and 1 files
+    if(planeCount >= MAX_PLANE_COUNT) {
+      std::cout << std::endl << "Maximum threshold reached: plane count = " << MAX_PLANE_COUNT << ". Forcing end of plane extraction." << std::endl;
+      break;
+    }
+  }
 
 }
 
